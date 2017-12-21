3466b5a7c06b2433106dfae7a594ae0ed4224c79
ajay <ajayandrodev@gmail.com>
2017-12-21 21:18:12 +0530

    added images

--- app/src/main/java/com/ajayrockstar/meterial/ajdevops/activities/MainActivity.kt
+++ app/src/main/java/com/ajayrockstar/meterial/ajdevops/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ajayrockstar.meterial.ajdevops
+package com.ajayrockstar.meterial.ajdevops.activities
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.ajayrockstar.meterial.ajdevops.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -60,35 +61,50 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.nav_linux -> {
-                Toast.makeText(this@MainActivity, "Its Linux", Toast.LENGTH_SHORT).show();
+                showToast("its linux");
+
 
             }
             R.id.nav_git -> {
+                showToast("its git");
 
             }
             R.id.nav_chef -> {
+                showToast("its chef");
 
             }
             R.id.nav_jenkins -> {
+                showToast("its jenkins");
 
             }
             R.id.nav_docker -> {
+                showToast("its docker");
+
             }
             R.id.nav_ansible -> {
+                showToast("its ansible");
 
             }
             R.id.nav_aws -> {
+                showToast("its aws");
 
             }
             R.id.nav_share -> {
+                showToast("its share");
 
             }
             R.id.nav_send -> {
+                showToast("its send");
 
             }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this@MainActivity, "" + msg, Toast.LENGTH_SHORT).show();
+
     }
 }
